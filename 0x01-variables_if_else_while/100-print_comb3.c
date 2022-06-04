4e7f7d682b03fb813cbeb5568210f5854f9d18da
tonyray2k420 <tonyray2k420@gmail.com>
2022-06-03 18:21:41 -0700

    variable_esle_while_task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,47 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Author: Olalekan js
+ * Nick  : .Files
+ * Date  : 12-03-2022
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	int out_c; /*** out_c: out_counter **/
+	int in_c;  /*** in_c: inner_counter **/
+
+	out_c = '0';
+
+	/*** OUTER LOOP ***/
+	while (out_c <= '9')
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+		in_c = '0';
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+		/*** INNER LOOP ***/
+		while (in_c <= '9')
+		{
+			if (in_c != out_c && !(in_c < out_c))
+			{
+				putchar(out_c);
+				putchar(in_c);
+
+				if (out_c != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			in_c++;
 		}
+
+		out_c++;
 	}
 
 	putchar('\n');
