898204b35f2e71b91799859b7420b0408bba4fbc
tonyray2k420 <tonyray2k420@gmail.com>
2022-06-03 17:44:25 -0700

    variableesle

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -16,34 +16,3 @@ int main(void)
 
 	return (0);
 }
-Esc
-:wq
-git add .
-git commit -m '2-print_alphabet.c'
-git push
-
-
-TASK 3
-vi 3-print_alphabets.c
-i
-#include <stdio.h>
-
-/**
- * main - Prints the alphabet in lowercase, and then in uppercase.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
-
-	putchar('\n');
-
-	return (0);
-}
