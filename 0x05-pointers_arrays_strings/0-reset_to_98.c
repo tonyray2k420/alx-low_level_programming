e77143e983661e5775def21d77dbda4d82dbb8b6
tonyray2k420 <tonyray2k420@gmail.com>
2022-06-13 13:30:14 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of given input argument.
+ * @n: paramete to reset_to_98 func
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	/**
+	 * Alter or change the passed in value to another:
+	 *
+	 * is done by REFERENCE.
+	 */
+
+	*n = 98;
+}
